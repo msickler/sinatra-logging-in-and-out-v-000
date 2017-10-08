24af355657f5c59ed4edf255458ab51e444994c7
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-08 01:16:51 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,9 @@
 ActiveRecord::Schema.define(version: 1) do
 
   create_table "users", force: :cascade do |t|
-    t.string  "username"
-    t.string  "password"
-    t.integer "balance"
+    t.string "username"
+    t.string "password"
+    t.float  "balance"
   end
 
 end
