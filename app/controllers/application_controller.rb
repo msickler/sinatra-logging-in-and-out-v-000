e4bce1ee18acda40e3804ce19cb705308c1907cc
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-08 01:10:53 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,8 @@ class ApplicationController < Sinatra::Base
      if @user != nil && @user.password == params[:password]
       session[:user_id] = @user.id
        redirect to '/account'
+     else
+    erb :error
      end
     end
 
