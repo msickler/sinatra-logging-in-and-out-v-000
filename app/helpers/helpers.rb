46bb46e159036bd3970636005f811aabb8d44381
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-08 01:12:40 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -4,7 +4,7 @@ class Helpers
           @user = User.find_by(id: session[:user_id])
           @user
       end
-  
+
       def self.is_logged_in?(session)
           !!User.find_by(id: session[:user_id])  ? true : false
       end
