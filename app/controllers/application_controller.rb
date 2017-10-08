972d470d47d39debecca3cbf73b02e18025aff64
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-08 01:10:06 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,9 +13,9 @@ class ApplicationController < Sinatra::Base
   post '/login' do
 
     @user = User.find_by(:username => params[:username])
-  
+
      if @user != nil && @user.password == params[:password]
-      session[:user_id] = @user.id 
+      session[:user_id] = @user.id
        redirect to '/account'
      end
     end
